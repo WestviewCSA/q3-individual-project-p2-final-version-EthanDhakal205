44329c4431ea3f1a58cd5fec8f8cfdccd8249b1f
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-03-22 22:31:13 -0700

    Add Comments and Constructor for Only A* algo in Position.java

--- Q3 Project 1 Period 2/src/Position.java
+++ Q3 Project 1 Period 2/src/Position.java
@@ -1,9 +1,12 @@
 public class Position {
+    //g is the cost from the start, which means it only gets used by the A*
+    //reference of parent psoition is stored so that the full path can be reconstructed
     public int level;
     public int row;
     public int col;
     public Position parent;
     public int g;
+
     
     public Position(int level, int row, int col, Position parent) {
         this.level = level;
@@ -12,6 +15,17 @@ public class Position {
         this.parent = parent;
         this.g = 0;
     }
+
+    //only a* uses this one because the g gets set
+    public Position(int level, int row, int col, Position parent, int g) {
+        this.level = level;
+        this.row = row;
+        this.col = col;
+        this.parent = parent;
+        this.g = g;
+    }
+
+    
     public boolean equals(Position o) {
       if(this==o) {
         return true;
